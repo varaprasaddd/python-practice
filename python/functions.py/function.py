3eb0f72349db901a711127c642b4c1f7a96bb96f
varaprasaddd <telagamsettivaraprasadreddy@gmail.com>
2026-07-30 23:14:32 +0530

    Add files via upload

--- python/functions.py/function.py
+++ python/functions.py/function.py
@@ -36,6 +36,96 @@ for i in range(1,11):
     print('factorial of {} is {}'.format(i,fact(i)))
 
 
+#default arguments
+def greet(name = "hii"):
+    print("hello",name)
+
+greet()
+greet("reddyy")
+
+
+#check whether number is prime or not
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    for i in range (2, n) :
+        if n % i == 0:
+         return False
+    
+
+    return True
+n = int(input("enter a num"))
+if is_prime(n):
+       
+    print("prime")
+else:
+    print("not a prime")
+
+#first 10 prime numbers
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    for i in range(2,num):
+        if num %i == 0:
+             return False
+    return True
+
+count = 0
+num = 2
+n = int(input("enter a numbber"))
+while count < n:
+    if is_prime(num):
+        print(num)
+        count+=1
+    num+=1
+
+#Variable Length Arguments (*args)
+def total(*numbers):
+    print(sum(numbers))
+
+total(10, 20)
+total(10, 20, 30, 40)
+
+#Keyword Variable Length Arguments (**kwargs)
+def details(**data):
+    for key, value in data.items():
+        print(key, value)
+
+details(name="Vara", age=20, city="Chennai")
+
+
+
+#Local Variable          	                     Global Variable
+#Declared inside a function	                     Declared outside all functions
+#Can be used only inside that function	         Can be used throughout the program
+#Exists only while the function is running	     Exists until the program ends
+
+#9. Local Variables
+def test():
+    x = 10
+    print(x)
+
+test()
+
+
+# Global Variables
+x = 100
+
+def show():
+    print(x)
+
+show()
+
+
+#Recursive Function
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))
 
 
 
