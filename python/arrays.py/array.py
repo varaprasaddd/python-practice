9a8b1e4e521ba322b74d4e94fd89a7080037ce2f
varaprasaddd <telagamsettivaraprasadreddy@gmail.com>
2026-08-07 22:02:21 +0530

    Add files via upload

---
+++ python/arrays.py/array.py
@@ -0,0 +1,47 @@
+'''
+#Arrays are nothing but a collection of elements of same datatype 
+names = ["ravi","reddy","saran","jaswanth","arun"]
+for i in names:
+    print(i)
+
+#accessing array
+arr = ["ravi","reddy","saran","jaswanth","arun"]
+print(arr[0])
+print(arr[-1])
+
+#updating array
+num = [1,2,3,4,5]
+num[2] = 5
+print(num)
+'''
+
+#adding and removing elements
+arr = [1,2,3,4,5]
+arr.append(4)
+arr.insert(2,7)
+arr.remove(1)
+print(arr)
+
+
+#slicing arrays
+arr = [1,2,3,4,5]
+print(arr[0:2])
+print(arr[::-1])
+print(arr[:-1])
+print(arr[0:5])
+print(arr[-2:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
